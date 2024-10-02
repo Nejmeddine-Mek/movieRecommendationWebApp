a5967763a8a13d835d4226f45b4a8dd208d23d0b
Nejmeddine-Mek <theotakugamer2005@gmail.com>
2024-10-02 23:25:06 +0100

    initial commit

---
+++ public/index.js
@@ -0,0 +1,6 @@
+const resrchInput = document.getElementsByClassName('input');
+const apiKey = `18ca1b4a`;
+const API = `htpp://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}`;
+
+
+setTimeout(3000, () => console.log(resrchInput.value))
