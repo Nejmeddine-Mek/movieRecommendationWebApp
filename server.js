2c9e1f24274062ae19058a903cd5c2176a620de6
Nejmeddine-Mek <theotakugamer2005@gmail.com>
2024-10-06 18:16:49 +0100

    navFixed

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const PORT = 3000;
+const apiKey = `18ca1b4a`;
+const API = `htpp://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}`;
 
 app.use(express.static('./public'))
 app.use(express.json())
